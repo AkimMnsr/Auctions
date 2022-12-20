ff1ee96aaf8a32029d2c517be417c661f9929a58
AkimMnsr <akim.mnasri@gmail.com>
2022-12-20 09:55:22 +0100

    ajout code erreur servlet

---
+++ Auctions/src/main/java/fr/eni/javaee/auctions/servlet/gestion_erreur/CodeErreurUtilisateur.java
@@ -0,0 +1,8 @@
+package fr.eni.javaee.auctions.servlet.gestion_erreur;
+
+public class CodeErreurUtilisateur {
+
+	public static final int FORMAT_PSEUDO_ALPHANUMERIC = 30_001;
+	public static final int UTILISATEUR_INCONNU = 30_002;
+	public static final int UTILISATEUR_DEJA_EXISTANT = 30_003;
+}
