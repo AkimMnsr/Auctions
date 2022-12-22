6db5c1ba9dfd509195978321c827247bb5e7305d
AkimMnsr <akim.mnasri@gmail.com>
2022-12-22 10:30:28 +0100

    push de modif léger type css

--- Auctions/src/main/java/fr/eni/javaee/auctions/dal/CodesErreursUtilisateurDAL.java
+++ Auctions/src/main/java/fr/eni/javaee/auctions/dal/CodesErreursUtilisateurDAL.java
@@ -2,7 +2,7 @@ package fr.eni.javaee.auctions.dal;
 
 public abstract class CodesErreursUtilisateurDAL {
 	
-	public static final int INSERT_OBJECT_NULL = 1000;
+	public static final int INSERT_OBJECT_NULL = 10000;
 	public static final int INSERT_OBJECT_ECHEC = 10001;
 	public static final int INSERT_PSEUDO_EXISTANT = 10002;
 	public static final int INSERT_EMAIL_EXISTANT = 10003;
